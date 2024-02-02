3188b3722a4734cbaeb2c597cc60ee11987938c5
Raj Khanke <156091970+khankeraj@users.noreply.github.com>
2024-02-02 17:32:46 +0530

    Add files via upload

---
+++ invoice-service/src/test/java/com/invoiceservice/invoiceservice/InvoiceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invoiceservice.invoiceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
